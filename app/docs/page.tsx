e4654e08a4ec769af68211504e5e9e54d48adc06
Zeeshan <zeeshanalidal500@gmail.com>
2025-05-18 09:06:41 +0500

    add LICENSE

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import DocsComponent from "@/components/DocsComponent";
+import DocsComponent from "@/components/docs/DocsComponent";
 
 export const metadata: Metadata = {
   title: "Documentation - SuperKit",
